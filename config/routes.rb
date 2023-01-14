7280bf3e5e03e3065188130b6f53d1013e3219c6
suebsaifi <sueb18-cs@sanskar.org>
2023-01-14 23:37:16 +0400

    Modify rails admin login functionality for admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  # authenticate :user, lambda { |u| u.has_role?(:admin) } do
+  #   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  # end
+
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
